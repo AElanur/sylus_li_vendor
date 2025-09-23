b922afeaef0611934c78ba237f566914f38c343a
Talon <ak1998@live.nl>
2025-09-23 21:06:53 +0200

    COMMANDS FINALLY WORK, uncomment what you commented - and keep refactoring your code, you genius

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import os
+import asyncio
 from dotenv import load_dotenv
 from client.sylus_bot_client import bot
+from cogs.selection_commands import SelectionCommands
 
 load_dotenv()
-from cogs import events
 
+async def main():
+    async with bot:
+        await bot.add_cog(SelectionCommands(bot))
+        await bot.start(os.getenv('DISCORD_KEY'))
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f'Logged in as {bot.user} ({bot.user.id})')
+    print('Commands synced and bot is ready.')
 
 if __name__ == "__main__":
-    bot.run(os.getenv('DISCORD_KEY'))
+    asyncio.run(main())
